a10952fc499a7261110f02d0fb69a0cf0d9e6419
dhananjay <dhananjaysharma2506@gmail.com>
2021-11-07 23:03:13 +0530

    value print by user

--- matrix.c
+++ matrix.c
@@ -2,19 +2,17 @@
 int main()
 {
     int a[3][3], i, j;
-    for (i =)
-        a[0][0] = 3;
-    a[0][1] = 2;
-    a[0][2] = 3;
-    a[1][0] = 6;
-    a[1][1] = 2;
-    a[1][2] = 6;
-    a[2][0] = 5;
-    a[2][1] = 6;
-    a[2][2] = 5;
+    printf("enter 9 value");
     for (i = 0; i <= 2; i++)
     {
-        printf("this is %d row\n", i);
+        for (j = 0; j <= 2; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (i = 0; i <= 2; i++)
+    {
+        // printf("this is %d row\n", i);
         for (j = 0; j <= 2; j++)
         {
             printf("%d ", a[i][j]);
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+
+        scanf("%c", &a[i]);
+        while (j <= 10)
+        {
+            printf("%c", a[i]);
+    
