c4fcc8ed7fb8fc3b065c4ea52a820941dc6aadcb
dhananjay <dhananjaysharma2506@gmail.com>
2021-11-07 23:16:25 +0530

    sum of two matricses

--- matrix.c
+++ matrix.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    int a[3][3], i, j;
-    printf("enter 9 value");
+    int a[3][3], b[3][3], c[3][3], i, j;
+    printf("enter first matrix 9 value");
     for (i = 0; i <= 2; i++)
     {
         for (j = 0; j <= 2; j++)
@@ -10,12 +10,21 @@ int main()
             scanf("%d", &a[i][j]);
         }
     }
+    printf("enter second matrix 9 value");
+    for (i = 0; i <= 2; i++)
+    {
+        for (j = 0; j <= 2; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
     for (i = 0; i <= 2; i++)
     {
         // printf("this is %d row\n", i);
         for (j = 0; j <= 2; j++)
         {
-            printf("%d ", a[i][j]);
+            c[i][j] = a[i][j] + b[i][j];
+            printf("%d", c[i][j]);
         }
         printf("\n");
     }
